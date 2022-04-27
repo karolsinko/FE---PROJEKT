dbc8a56eaae1b84eb1b378b2f39221fed78f4225
karolsinko <karol.sinko30@gmail.com>
2022-04-27 18:53:12 +0200

    Evidencia očkovanosti 2. - takmer hotová (dokoncit formatovanie)

--- src/app/ockovanost/ockovanost-stranka/ockovanost-stranka.component.ts
+++ src/app/ockovanost/ockovanost-stranka/ockovanost-stranka.component.ts
@@ -4,6 +4,8 @@ import {ZoznamVakcin} from "../../models/vakcina.model";
 import {ZoznamOsob} from "../../models/osoba.model";
 import {OsobaServiceService} from "../../../Service/osoba-service.service";
 import {VakcinaServiceService} from "../../../Service/vakcina-service.service";
+import {OckovanostServiceService} from "../../../Service/ockovanost-service.service";
+import {ZoznamOckovanosti} from "../../models/ockovanost.model";
 
 @Component({
   selector: 'app-ockovanost-stranka',
@@ -12,15 +14,11 @@ import {VakcinaServiceService} from "../../../Service/vakcina-service.service";
 })
 export class OckovanostStrankaComponent implements OnInit {
 
-  constructor(private router: Router, private osobaService: OsobaServiceService, private vakcinaService: VakcinaServiceService) { }
+  constructor(private router: Router, private osobaService: OsobaServiceService, private vakcinaService: VakcinaServiceService, private ockovanostService: OckovanostServiceService) { }
 
   vakciny: ZoznamVakcin[] = [];
   osoby: ZoznamOsob[] = [];
-
-  ngOnInit(): void {
-    this.obnovitOsoby();
-    this.obnovitVakciny();
-  }
+  ockovania: ZoznamOckovanosti[] = [];
 
   chodSpat(): void {
     this.router.navigate(['']);
@@ -32,10 +30,24 @@ export class OckovanostStrankaComponent implements OnInit {
       this.osoby = data;
     });
   }
+
   obnovitVakciny(): void {
     this.vakcinaService.getVakciny().subscribe(data => {
       console.log('Prislo: ', data);
       this.vakciny = data;
     });
+  }
+
+  obnovitOckovanie(): void {
+    this.ockovanostService.getOckovanosti().subscribe(data => {
+      console.log('Prislo: ', data);
+      this.ockovania = data;
+    });
+  }
+
+  ngOnInit(): void {
+    this.obnovitOsoby();
+    this.obnovitVakciny();
+    this.obnovitOckovanie();
   }
 }
