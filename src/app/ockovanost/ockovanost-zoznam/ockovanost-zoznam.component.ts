a699a1329ad41d85ab87f66d5602d52ceb48050c
karolsinko <karol.sinko30@gmail.com>
2022-05-01 13:03:05 +0200

    Fixnutie podla codereview 3

--- src/app/ockovanost/ockovanost-zoznam/ockovanost-zoznam.component.ts
+++ src/app/ockovanost/ockovanost-zoznam/ockovanost-zoznam.component.ts
@@ -1,8 +1,7 @@
 import {Component, Input} from '@angular/core';
-import {Vakcina, ZoznamVakcin} from "../../models/vakcina.model";
-import {Osoba, ZoznamOsob} from "../../models/osoba.model";
-import {Ockovanost, ZoznamOckovanosti} from "../../models/ockovanost.model";
-import {FormControl, FormGroup} from "@angular/forms";
+import {ZoznamVakcin} from "../../models/vakcina.model";
+import {ZoznamOsob} from "../../models/osoba.model";
+import {ZoznamOckovanosti} from "../../models/ockovanost.model";
 
 @Component({
   selector: 'app-ockovanost-zoznam',
@@ -10,23 +9,6 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./ockovanost-zoznam.component.css']
 })
 export class OckovanostZoznamComponent {
-  zoznam: FormGroup;
-
-  constructor() {
-    this.zoznam = new FormGroup({
-      id: new FormControl(null),
-      idOsoba: new FormControl(null),
-      idVakcina: new FormControl(null),
-      datumOckovania: new FormControl(null),
-    });
-  }
-
-  private fillForm(vakcina: Vakcina, osoba: Osoba, ockovanost: Ockovanost): void {
-    this.zoznam.controls['id'].setValue(ockovanost.id)
-    this.zoznam.controls['idOsoba'].setValue(osoba.id)
-    this.zoznam.controls['idVakcina'].setValue(vakcina.id)
-    this.zoznam.controls['datumOckovania'].setValue(ockovanost.datumOckovania)
-  }
 
   @Input()
   vakciny: ZoznamVakcin[] = [];
